f5cc850376befadacd026aa9d17baaf8d48df76c
naleilan <leilan.n@gmail.com>
2025-05-07 09:50:04 +0330

    splited components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -118,6 +118,7 @@ function ListBox() {
       <button className="btn-toggle" onClick={() => setIsOpen1(open => !open)}>
         {isOpen1 ? '–' : '+'}
       </button>
+
       {isOpen1 && <MovieList />}
     </div>
   );
@@ -129,18 +130,24 @@ function MovieList() {
   return (
     <ul className="list">
       {movies?.map(movie => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>🗓</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <Movie movie={movie} />
       ))}
     </ul>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
