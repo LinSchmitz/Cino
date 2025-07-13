4a1ebfea95d3c02a71e67a170648d7e66e825612
naleilan <leilan.n@gmail.com>
2025-07-13 08:47:55 +0330

    fixed Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,12 +3,11 @@ import './Loader.css';
 
 export default function Loader() {
   return (
-    <>
-      <div className="loader-circle">
-        <svg viewBox="0 0 80 80">
-          <circle r="32" cy="40" cx="40" id="test" />
-        </svg>
-      </div>
-    </>
+    <div className="loader-circle">
+      <span className="dot" />
+      <svg viewBox="0 0 160 160">
+        <circle r="64" cy="80" cx="80" />
+      </svg>
+    </div>
   );
 }
