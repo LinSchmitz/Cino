93880188dd99c9719637812b0ddfddd09a9e9440
naleilan <leilanschmitz@gmail.com>
2025-07-24 15:36:09 +0330

    Added useRef

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,7 +11,14 @@ export function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(function () {
-    inputEl.current.focus();
+    function callback(e) {
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback);
+    return () => document.addEventListener('keydown', callback);
   }, []);
 
   return (
